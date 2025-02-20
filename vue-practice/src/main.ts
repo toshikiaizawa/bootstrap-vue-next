00d564e7c6086f1d5375eec8875cb35d474a81d0
toshikiaizawa <aizawatoshiki129@gmail.com>
2025-02-20 23:44:05 +0900

    storybookの追加

--- vue-practice/src/main.ts
+++ vue-practice/src/main.ts
@@ -2,6 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {createBootstrap} from 'bootstrap-vue-next'
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 
 import App from './App.vue'
 import router from './router'
