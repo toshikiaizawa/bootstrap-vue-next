3c27ca60c1e18d8cb360d8a7913ab8390497c016
toshikiaizawa <aizawatoshiki129@gmail.com>
2025-02-20 23:44:51 +0900

    modalを作成して表示できるようにした

---
+++ vue-practice/src/components/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+  <BButton @click="$emit('update:modelValue', !modelValue)">Toggle modal</BButton>
+  <BModal :model-value="modelValue" title="Hello, World!" @hide="$emit('update:modelValue', false)">
+    Foobar?
+  </BModal>
+</template>
+
+<script setup lang="ts">
+import { BModal } from 'bootstrap-vue-next';
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{ modelValue: boolean }>();
+defineEmits(['update:modelValue']);
+</script>
--- vue-practice/src/views/AboutView.vue
+++ vue-practice/src/views/AboutView.vue
@@ -1,15 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
+
+const modal = ref(false);
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="modal = !modal">押すとモーダルが表示できる</button>
   </div>
+  <Modal v-model="modal"></Modal>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
